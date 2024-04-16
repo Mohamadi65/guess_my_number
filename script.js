20cb367da40a24db7d21fbeb48ea0d15c57802c7
Mohamadi65 <freelancemohamadi65@gmail.com>
2024-04-16 12:26:56 +0200

    first commit with the starter file

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const message = document.querySelector('.message');
+message.textContent = "😉 Correct Number!";
+
+document.querySelector('.number').textContent  = 13;
+document.querySelector('.score').textContent = 10;
+
+const guess = document.querySelector('.guess').value =23;
+console.log(guess);
+
